4ab87e22c69c27cbec9deb6357d4133f9ac52b6b
Fernando-php <fortiz@iesruizgijon.com>
2023-02-06 16:02:11 +0100

    modificacion Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Articulo::factory(30)->create();
+        $articulo = new Articulo();
+        $articulo->titulo='Como aprobar DWES y no morir en el intento';
+        $articulo->fecha='2023-02-06';
+        $articulo->contenido='El truco es sencillo, solo tienes que practicar mucho y llevar todos los conocimientos al dia';
+        $articulo->save();
     }
 }
