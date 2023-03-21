afafb1ff22b969c819812914122f5e572e39f8f1
Fernando-php <fortiz@iesruizgijon.com>
2023-03-21 09:14:51 +0100

    Modificación título en Inicio

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -10,7 +10,7 @@
 </head>
 <body>
   <div class="contenedor">
-    <h1>Mi blog personal en laravel</h1>
+    <h1>Articulos de Tecnología</h1>
     @yield('content')
     <br><a class="titulo" href="{{route('inicio')}}">Volver a Inicio</a>
   </div>
